2c486971a26b50e92dbae99c0e99d328dd89d0e2
gwoosoft <71423299+gwoosoft@users.noreply.github.com>
2021-07-27 15:12:47 -0400

    Create maxArea.cpp

---
+++ maxArea.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {   
+        int left=0;
+        int right=height.size()-1;
+        int sz=height.size();
+        int mx=0;
+        int csize=0;
+        
+        while(left<sz&&right>0){
+            mx=max(mx,(right-left)*min(height[left],height[right]));
+            if(height[left]<height[right]) {left++;}
+            else {right--;}
+        }
+
+        return mx;
+    }
+};
