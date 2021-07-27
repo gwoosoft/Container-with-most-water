1315e24dad5143f7ec41baac66406cdaffac38cb
gwoosoft <71423299+gwoosoft@users.noreply.github.com>
2021-07-27 15:14:15 -0400

    Update maxArea.cpp

--- maxArea.cpp
+++ maxArea.cpp
@@ -5,7 +5,6 @@ public:
         int right=height.size()-1;
         int sz=height.size();
         int mx=0;
-        int csize=0;
         
         while(left<sz&&right>0){
             mx=max(mx,(right-left)*min(height[left],height[right]));
